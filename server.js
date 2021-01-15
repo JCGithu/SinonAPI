0068cdfd56071ee6ba1a3498cf8826405877d980
jcgithu <jack.c.gracie@gmail.com>
2021-01-15 16:10:23 +0000

    added youtube

--- server.js
+++ server.js
@@ -5,6 +5,46 @@ const morgan = require('morgan');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+async function yt(ytURL) {
+  let info = await ytdl.getInfo(ytURL);
+  let output = {};
+  let i = 1;
+  for (const format in info.formats) {
+    var obj = info.formats[format];
+    let addInfo = '';
+    let endInfo = '';
+    if (!obj.hasAudio) {
+      addInfo = ' (Only video)';
+    }
+    if (!obj.hasVideo) {
+      addInfo = ' (Only audio)';
+    }
+    if (obj.isLive) {
+      obj.quality = 'livestream';
+    }
+    if (obj.qualityLabel == null) {
+      obj.qualityLabel = '';
+    }
+    if (obj.fps == undefined) {
+      obj.fps = '';
+    } else {
+      endInfo = `// ${obj.fps}p`;
+    }
+    if (obj.quality !== 'tiny') {
+      output[i] = {};
+      output[i].text = `${obj.quality}:${addInfo} ${obj.qualityLabel} ${obj.container} ${endInfo}`;
+      output[i].url = obj.url;
+    }
+    i++;
+    if (i > info.formats.length) {
+      return output;
+    }
+  }
+}
+
 const port = process.env.PORT || 500;
 
 const app = express();
@@ -56,6 +96,11 @@ app.get('/parliament/:token', cors(corsOptions), async (req, res) => {
       return livestreamURL;
     });
   res.json(HLS);
+});
+
+app.get('/youtube/:token', cors(corsOptions), async (req, res) => {
+  let info = await yt(req.params.token);
+  res.json(info);
 });
 
 app.listen(port, () => {
