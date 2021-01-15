74fac3d9a1a0672d8f1c2b46c3da265f2939c197
jcgithu <jack.c.gracie@gmail.com>
2021-01-15 12:07:39 +0000

    added parliament tv

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const fetch = require('node-fetch');
 require('dotenv').config();
 
-const port = process.env.PORT || 500;
+const port = 500;
 
 const app = express();
 app.use(helmet());
@@ -13,24 +13,41 @@ app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 
+const fetchSets = {
+  referrer: '',
+  method: 'GET',
+  origin: '*',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  dataType: 'json',
+};
+
 app.get('/periscope/:token', async (req, res) => {
   let token = req.params.token;
   let URL = `https://api.periscope.tv/api/v2/getAccessPublic?token=${token}`;
-  let HLS = await fetch(URL, {
-    referrer: '',
-    method: 'GET',
-    origin: '*',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    dataType: 'json',
-  })
+  let HLS = await fetch(URL, fetchSets)
     .then((data) => {
       return data.json();
     })
     .then((json) => {
       console.log(json);
       return json.hls_url;
+    });
+  res.json(HLS);
+});
+
+app.get('/parliament/:token', async (req, res) => {
+  let token = req.params.token;
+  let URL = `http://videoplayback.parliamentlive.tv/Player/Live/${token}`;
+  let HLS = await fetch(URL, fetchSets)
+    .then((data) => {
+      return data.text();
+    })
+    .then((text) => {
+      var regex = /[0-9A-Za-z\/\.\-\:]+.m3u8/g;
+      var livestreamURL = text.match(regex)[0];
+      return livestreamURL;
     });
   res.json(HLS);
 });
