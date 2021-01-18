51599df1c13606a52b7874b3c2d0a3d7a8d55bf7
jcgithu <jack.c.gracie@gmail.com>
2021-01-18 10:27:20 +0000

    improved filters

--- workers/youtube.js
+++ workers/youtube.js
@@ -8,26 +8,35 @@ async function gate(obj) {
     if (obj.container !== 'ts') {
       return false;
     }
+  }
+  if (obj.qualityLabel !== null) {
     let height = parseInt(obj.qualityLabel.split('p')[0]);
     if (height < 300) {
       return false;
-    } else {
-      return true;
     }
-  } else {
-    return true;
   }
+  if (obj.quality == 'tiny' && !obj.hasAudio) {
+    return false;
+  }
+  return true;
 }
 
 async function filter(i, obj, output) {
+  output[i] = {};
   if (obj.isLive) {
     obj.quality = 'livestream';
+    output[i].live = true;
   }
   if (!obj.hasAudio) {
     addInfo = ' (Only video)';
+    output[i].onlyVideo = true;
   }
   if (!obj.hasVideo) {
     addInfo = ' (Only audio)';
+    output[i].onlyAudio = true;
+  }
+  if (obj.hasVideo && obj.hasAudio) {
+    output[i].full = true;
   }
   if (obj.qualityLabel == null) {
     obj.qualityLabel = '';
@@ -37,28 +46,24 @@ async function filter(i, obj, output) {
   } else {
     endInfo = `// ${obj.fps}p`;
   }
-  if (obj.quality !== 'tiny') {
-    output[i] = {};
-    output[i].text = `${addInfo} ${obj.qualityLabel} ${obj.container} ${endInfo}`;
-    output[i].url = obj.url;
-  }
+  output[i].text = `${addInfo} ${obj.qualityLabel} ${obj.container} ${endInfo}`;
+  output[i].url = obj.url;
   return output;
 }
 
 async function yt(ytURL) {
   let info = await ytdl.getInfo(ytURL);
   let output = {};
-  let i = 1;
   for (const format in info.formats) {
     var obj = info.formats[format];
     let objGate = await gate(obj);
     if (objGate) {
-      let objFiltered = await filter(i, obj, output);
-      output = objFiltered;
-    }
-    i++;
-    if (i > info.formats.length) {
-      return output;
+      let fullOutput = await filter(format, obj, output);
+      console.log(format);
+      console.log(info.formats.length);
+      if (format == info.formats.length - 1) {
+        return fullOutput;
+      }
     }
   }
 }
